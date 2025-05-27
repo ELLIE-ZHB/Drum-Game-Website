2f3cebc52b712a5377d08c325a535680acc6615d
Elaheh Zehtab <elahehzehtab20003@gmail.com>
2025-05-27 15:33:16 -0400

    the drum game website!

---
+++ index.js
@@ -0,0 +1,8 @@
+for (var i =0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelector(".set button")[i].addEventListener("click", function (){
+    alert("i got clicked!");
+});
+}
+
+
+
